8a8c17f51bfed3a765cf1512842ce5d8c09bb3c6
함지현 <gkawlgus01@naver.com>
2024-12-20 10:58:24 +0900

    update: 포스트 JSON 변환

--- sns/src/app.controller.ts
+++ sns/src/app.controller.ts
@@ -1,12 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'ham._.ji',
+      title: '함지현',
+      content: '안녕하세요. 함지현입니다.',
+      likeCount: 100,
+      commentCount: 3,
+    };
   }
 }
