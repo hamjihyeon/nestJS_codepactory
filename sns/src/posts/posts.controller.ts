2b2138e077b6848f5921ea8374a98d27e3d2be93
함지현 <gkawlgus01@naver.com>
2024-12-20 13:43:28 +0900

    update: GET posts 구현하기

--- sns/src/posts/posts.controller.ts
+++ sns/src/posts/posts.controller.ts
@@ -9,7 +9,8 @@ import { PostsService } from './posts.service';
  * commentCount: number;
  */
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -17,18 +18,55 @@ interface Post {
   commentCount: number;
 }
 
+let posts : PostModel[] = [
+  {
+    id: 1,
+    author: 'ham._.ji',
+    title: '함지현',
+    content: '안녕하세요. 함지현입니다.',
+    likeCount: 100,
+    commentCount: 3,
+  },
+  {
+    id: 2,
+    author: 'jihyeon',
+    title: '지현',
+    content: '안녕하세요. 지현입니다.',
+    likeCount: 100,
+    commentCount: 3,
+  },
+  {
+    id: 3,
+    author: 'hamji',
+    title: '함지',
+    content: '안녕하세요. 함지입니다.',
+    likeCount: 100,
+    commentCount: 3,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) GET /posts
+  //    모든 posts를 다 가져온다.
   @Get()
-  getPost(): Post {
-    return {
-      author: 'ham._.ji',
-      title: '함지현',
-      content: '안녕하세요. 함지현입니다.',
-      likeCount: 100,
-      commentCount: 3,
-    };
+  getPosts() {
+    return posts;
   }
+
+  // 2) GET /posts/:id
+  //    id에 해당되는 post를 가져온다.
+  //    ex) id = 1인 경우 id가 1인 post를 가져온다.
+
+  // 3) POST /posts
+  //    post를 생성한다.
+
+  // 4) PUT /posts/:id
+  //    id에 해당되는 POST를 변경한다.
+
+  // 5) DELETE /posts/:id
+  //    id에 해당되는 POST를 삭제한다.
+
 }
