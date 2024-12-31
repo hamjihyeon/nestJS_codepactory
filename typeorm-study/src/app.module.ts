ed0593cd735e5485eb2b10a8856d08bf1fcd31da
함지현 <gkawlgus01@naver.com>
2024-12-31 18:52:15 +0900

    feat: table inheritance 사용하기

--- typeorm-study/src/app.module.ts
+++ typeorm-study/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from './entity/user.entity';
 import { StudentModel, TeacherModel } from './entity/person.entity';
+import { AirplaneModel, BookModel, CarModel, ComputerModel, SingleBaseModel } from './entity/inheritance.entity';
 
 @Module({
   imports: [
@@ -21,6 +22,11 @@ import { StudentModel, TeacherModel } from './entity/person.entity';
         UserModel,
         StudentModel,
         TeacherModel,
+        BookModel,
+        CarModel,
+        SingleBaseModel,
+        ComputerModel,
+        AirplaneModel, 
       ],
       synchronize: true,
     })
