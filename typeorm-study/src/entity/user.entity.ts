a9472af518a271bf8cbf1b1d1879badfd0be2189
함지현 <gkawlgus01@naver.com>
2025-01-13 16:40:54 +0900

    update: find 옵션들들 공부

--- typeorm-study/src/entity/user.entity.ts
+++ typeorm-study/src/entity/user.entity.ts
@@ -76,7 +76,7 @@ export class UserModel {
 
     @OneToOne(()=> ProfileModel, (profile) => profile.user, {
         // find() 실행 할 때마다 항상 같이 가져올 relation
-        eager: true,
+        eager: false,
         // 저장할 때 relation을 한번에 같이 저장 가능
         cascade: true,
         // null이 가능한지
