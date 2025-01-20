99e8fc950de81ef5d78a56a4c1dcf0e287acb4e2
함지현 <gkawlgus01@naver.com>
2025-01-20 17:13:48 +0900

    fix: 에러 해결

--- sns/src/posts/posts.controller.ts
+++ sns/src/posts/posts.controller.ts
@@ -23,11 +23,11 @@ export class PostsController {
   //    post를 생성한다.
   @Post()
   postPosts(
-    @Body('author') author: string,
+    @Body('authorId') authorId: number,
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    return this.postsService.createPost(author, title, content);
+    return this.postsService.createPost(authorId, title, content);
   }
 
   // 4) Patch /posts/:id
@@ -35,11 +35,10 @@ export class PostsController {
   @Patch(':id')
   patchPost(
     @Param('id') id: string,
-    @Body('author') author?: string,
     @Body('title') title?: string,
     @Body('content') content?: string,
   ){
-    return this.postsService.updatePost(+id, author, title, content);
+    return this.postsService.updatePost(+id, title, content);
   }
 
   // 5) DELETE /posts/:id
--- sns/src/posts/posts.service.ts
+++ sns/src/posts/posts.service.ts
@@ -74,12 +74,14 @@ export class PostsService {
         return post;
     }
 
-    async createPost(author: string, title: string, content: string) {
+    async createPost(authorId: number, title: string, content: string) {
       // 1. create -> 저장할 객체를 생성
       // 2. save -> 객체를 저장(create 매서드에서 생성한 객체로)
 
       const post = this.postsRepository.create({
-        author,
+        author: {
+          id: authorId, 
+        },
         title,
         content,
         likeCount: 0,
@@ -91,7 +93,7 @@ export class PostsService {
       return newPost;
     }
 
-    async updatePost(postID: number, author?: string, title?: string, content?: string) { 
+    async updatePost(postID: number, title?: string, content?: string) { 
       // save의 기준
       // 1. 만약에 데이터가 존재하지 않는다면 (id 기준) 새로 생성
       // 2. 만약에 데이터가 존재한다면 (같은 id의 값이 존재한다면) 존재하던 값을 업데이트
@@ -104,11 +106,6 @@ export class PostsService {
 
         if (!post) {
         throw new NotFoundException();
-        }
-
-        // 입력한 값들만 변경
-        if (author) {
-        post.author = author;
         }
 
         if (title) {
